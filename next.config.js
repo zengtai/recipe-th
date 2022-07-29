78ee1af22243ebaa9339d6cb279978ca9d340904
zengtai <zengtai@gmail.com>
2022-07-29 19:13:01 +0800

    广告预览

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ const nextConfig = {
     // You can, for example, get the latest git commit hash here
     return "20220729"; //
   },
-  basePath: "/webs/recipe/public/th",
+  // basePath: "/webs/recipe/public/th",
   trailingSlash: true,
 };
 
