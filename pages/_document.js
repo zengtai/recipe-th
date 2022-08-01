490d3a0685d035f3347c6ab4ce1ae0259e3f8219
Tai Zeng <zengtai@gmail.com>
2022-08-01 15:51:34 +0800

    广告测试 +1

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
 import { ADS_ID } from "../lib/constants";
 
 class MyDocument extends Document {
@@ -8,17 +9,38 @@ class MyDocument extends Document {
   }
 
   render() {
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        let adsLength = document.querySelectorAll(
+          ".adsbygoogle:only-child"
+        ).length;
+        for (let i = 0; i < adsLength; i++) {
+          try {
+            (window.adsbygoogle || []).push({});
+          } catch (e) {
+            console.error(`Adsense Error: `, e);
+          }
+        }
+        // try {
+        //   (window.adsbygoogle || []).push({});
+        // } catch (e) {
+        //   console.error(`Adsense Error: `, e);
+        // }
+      }
+    }, []);
     return (
       <Html lang="th">
         <Head>
-        <script
-        id="adsense-init"
-        onError={ (e) => { console.error('Script failed to load', e) }}
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADS_ID}`}
-        crossOrigin="anonymous"
-      />
-      </Head>
+          <script
+            id="adsense-init"
+            onError={(e) => {
+              console.error("Script failed to load", e);
+            }}
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADS_ID}`}
+            crossOrigin="anonymous"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
