ba32355129be8519ecfdd78f12c0eececfc7b504
Tai Zeng <zengtai@gmail.com>
2022-08-01 17:19:36 +0800

    简化

--- components/Banner.js
+++ components/Banner.js
@@ -31,7 +31,8 @@ const Banner = ({
       }
     }
   }, []);
-  return auto ? (
+
+  return (
     <div className={`${className} custom-ad`}>
       <ins
         className="adsbygoogle"
@@ -45,27 +46,45 @@ const Banner = ({
         data-adtest="on"
       />
       {/* <script dangerouslySetInnerHTML={{__html:`
-          (adsbygoogle = window.adsbygoogle || []).push({});    
-      `}} /> */}
-    </div>
-  ) : (
-    <div className={`${className} custom-ad`}>
-      <ins
-        className={`adsbygoogle`}
-        style={style}
-        data-ad-layout={layout}
-        data-ad-format={format}
-        data-ad-client={client}
-        data-ad-slot={slot}
-        data-ad-layout-key={layoutKey}
-        data-full-width-responsive={responsive}
-        data-adtest="on"
-      />
-      {/* <script dangerouslySetInnerHTML={{__html:`
-          (adsbygoogle = window.adsbygoogle || []).push({});    
-      `}} /> */}
+           (adsbygoogle = window.adsbygoogle || []).push({});
+       `}} /> */}
     </div>
   );
+  // return auto ? (
+  //   <div className={`${className} custom-ad`}>
+  //     <ins
+  //       className="adsbygoogle"
+  //       style={style}
+  //       data-ad-layout={layout}
+  //       data-ad-format={format}
+  //       data-ad-client={client}
+  //       data-ad-slot={slot}
+  //       data-ad-layout-key={layoutKey}
+  //       data-full-width-responsive={responsive}
+  //       data-adtest="on"
+  //     />
+  //     {/* <script dangerouslySetInnerHTML={{__html:`
+  //         (adsbygoogle = window.adsbygoogle || []).push({});
+  //     `}} /> */}
+  //   </div>
+  // ) : (
+  //   <div className={`${className} custom-ad`}>
+  //     <ins
+  //       className={`adsbygoogle`}
+  //       style={style}
+  //       data-ad-layout={layout}
+  //       data-ad-format={format}
+  //       data-ad-client={client}
+  //       data-ad-slot={slot}
+  //       data-ad-layout-key={layoutKey}
+  //       data-full-width-responsive={responsive}
+  //       data-adtest="on"
+  //     />
+  //     {/* <script dangerouslySetInnerHTML={{__html:`
+  //         (adsbygoogle = window.adsbygoogle || []).push({});
+  //     `}} /> */}
+  //   </div>
+  // );
 };
 
 export default Banner;
